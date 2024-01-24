da4492d71928277e76595ea3e443f2ab0ccd4a53
keedatesting <namratagaur14021996@gmail.com>
2024-01-25 02:45:08 +0530

    adding register test cases of automation website

--- cypress/integration/examples/Automation_excercise.js
+++ cypress/integration/examples/Automation_excercise.js
@@ -2,6 +2,6 @@ describe('My First Test', () => {
     it('Automation', () => {
       cy.visit('https://automationexercise.com/products')
       cy.get('.features_items  .col-sm-4').as('allproduts')
-      cy.get('@allproducts').find(':nth-child(7) > .product-image-wrapper > .single-products > .productinfo > h2').text()
+      
     })
   })
